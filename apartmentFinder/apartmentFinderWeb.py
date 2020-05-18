d7b5974133999c7881ccbd9d3a3a4d3059558e13
Aidan <aklane.work@gmail.com>
2020-05-18 14:18:35 -0400

    made apartment collection into functions

--- apartmentFinder/apartmentFinderWeb.py
+++ apartmentFinder/apartmentFinderWeb.py
@@ -2,27 +2,28 @@ from bs4 import BeautifulSoup as soup
 import requests
 
 my_url = "https://www.apartmentfinder.com/Michigan/East-Lansing-Apartments/Castle-Pointe-Apartments"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 bedrooms = 1
 
-uClient = requests.get(my_url,headers = headers)
-page_html = uClient.content
+def find_data(beds, url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    uClient = requests.get(url, headers=headers)
+    page_html = uClient.content
 
-bed_val = "bed"+str(bedrooms)
-page_soup = soup(page_html, "html.parser")
+    bed_val = "bed" + str(beds)
+    page_soup = soup(page_html, "html.parser")
 
-address = page_soup.find('div', {'class': "address"})
-address = address.span.string
-print(address)
+    address = page_soup.find('div', {'class': "address"})
+    address = address.span.string
 
-apartments = page_soup.find('div', {"data-tab-content-id":bed_val})
-available = apartments.div
+    apartments = page_soup.find('div', {"data-tab-content-id": bed_val})
+    available = apartments.div
 
-rooms = [item for item in available.select('div[data-beds=\"'+str(bedrooms)+'\"]')]
-print(rooms[0]['data-maxrent'])
+    rooms = [item for item in available.select('div[data-beds=\"' + str(beds) + '\"]')]
 
-to_rent = [address]
-for item in rooms:
-    to_rent.append({item['data-rentalkey']:[item['data-maxrent'],item['data-model']]})
+    to_rent = [address]
+    for item in rooms:
+        to_rent.append({item['data-rentalkey']: [item['data-maxrent'], item['data-model']]})
 
-print(to_rent)
+    return to_rent
+
+print(find_data(bedrooms, my_url))
--- apartmentFinder/apartmentsWeb.py
+++ apartmentFinder/apartmentsWeb.py
@@ -2,35 +2,36 @@ from bs4 import BeautifulSoup as soup
 import requests
 
 my_url = "https://www.apartments.com/castle-pointe-east-lansing-mi/ybf975v/"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 bedrooms = 1
 
-uClient = requests.get(my_url,headers = headers)
-page_html = uClient.content
+def find_data(beds, url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    uClient = requests.get(url, headers=headers)
+    page_html = uClient.content
 
-bed_val = "bed" + str(bedrooms)
-page_soup = soup(page_html, "html.parser")
-addressTag = page_soup.find('div', {'class': "propertyAddress"})
-addressTag = addressTag.h2
+    bed_val = "bed" + str(beds)
+    page_soup = soup(page_html, "html.parser")
+    addressTag = page_soup.find('div', {'class': "propertyAddress"})
+    addressTag = addressTag.h2
 
-address = ""
-for child in addressTag.contents:
-    if child.string.strip() != ",":
-        address += " " + child.string.strip()
-    else:
-        address += child.string.strip()
-address = address.strip()
-print(address)
+    address = ""
+    for child in addressTag.contents:
+        if child.string.strip() != ",":
+            address += " " + child.string.strip()
+        else:
+            address += child.string.strip()
+    address = address.strip()
 
-apartments = page_soup.find('div', {'data-tab-content-id':bed_val})
+    apartments = page_soup.find('div', {'data-tab-content-id': bed_val})
 
-available = apartments.div.table.tbody
+    available = apartments.div.table.tbody
 
+    rooms = [item for item in available.select('tr[data-beds=\"' + str(beds) + '\"]')]
 
-rooms = [item for item in available.select('tr[data-beds=\"'+str(bedrooms)+'\"]')]
+    to_rent2 = [address]
+    for item in rooms:
+        to_rent2.append({item['data-rentalkey']: [item['data-maxrent'], item['data-model']]})
 
-to_rent2 = [address]
-for item in rooms:
-    to_rent2.append({item['data-rentalkey']:[item['data-maxrent'], item['data-model']]})
+    return to_rent2
 
-print(to_rent2)
+print(find_data(bedrooms, my_url))
